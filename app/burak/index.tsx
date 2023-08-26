56485a0232768a6c5bf377aa7ee18f9331faacdc
burak <burak.ergen@outlook.com.tr>
2023-08-26 19:54:48 +0300

    First Commit

---
+++ app/burak/index.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function BurakPage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            slug
+        </main>
+    )
+}
